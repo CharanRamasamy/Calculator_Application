6f56891cc1463fa52737763671477175816d24eb
Sricharan Ramasamy <sr721595@dal.ca>
2018-02-25 08:54:31 -0400

    Button Operations almost done
    Completed most of the button operation in display

--- app/src/main/java/calculator/charan/com/MainActivity.java
+++ app/src/main/java/calculator/charan/com/MainActivity.java
@@ -2,12 +2,185 @@ package calculator.charan.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String displayValue = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button AC = findViewById(R.id.buttonAC);
+        Button Button0 = findViewById(R.id.button0);
+        Button Button1 = findViewById(R.id.button1);
+        Button Button2 = findViewById(R.id.button2);
+        Button Button3 = findViewById(R.id.button3);
+
+        Button Button4 = findViewById(R.id.button4);
+        Button Button5 = findViewById(R.id.button5);
+        Button Button6 = findViewById(R.id.button6);
+        Button Button7 = findViewById(R.id.button7);
+        Button Button8 = findViewById(R.id.button8);
+
+        Button Button9 = findViewById(R.id.button9);
+        Button ButtonC = findViewById(R.id.buttonC);
+        Button ButtonRoot = findViewById(R.id.buttonRoot);
+        Button ButtonPlus = findViewById(R.id.buttonPlus);
+        Button ButtonMul = findViewById(R.id.buttonMul);
+
+        Button ButtonPlusOrMinus = findViewById(R.id.buttonPlusOrMinus);
+        Button ButtonMin = findViewById(R.id.buttonMin);
+        Button ButtonEquals = findViewById(R.id.buttonEquals);
+        Button ButtonDot = findViewById(R.id.buttonDot);
+        Button ButtonDiv = findViewById(R.id.buttonDiv);
+
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+}
+
+    public void buttonNumberOperation(View view) {
+        TextView textView1 = findViewById(R.id.textView1);
+        switch(view.getId()){
+            case R.id.button1:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "1";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button2:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "2";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button3:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "3";
+                    textView1.setText(displayValue);
+                }
+                    break;
+
+            case R.id.button4:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "4";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button5:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "5";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button6:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "6";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button7:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "7";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button8:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "8";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button9:
+                if(displayValue.length()<10) {
+                    displayValue = displayValue + "9";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.button0:
+                if(displayValue.length()<10 && !displayValue.equals("")) {
+                    displayValue = displayValue + "0";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+
+        }
     }
+
+
+    public void buttonClearOperation(View view) {
+        TextView textView1 = findViewById(R.id.textView1);
+        switch (view.getId()) {
+            case R.id.buttonC:
+                if (displayValue.length() < 10 && displayValue.length()>=1) {
+                    displayValue = displayValue.substring(0,displayValue.length()-1);
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.buttonAC:
+                displayValue = "";
+                textView1.setText(R.string.Zero);
+                break;
+        }
+    }
+
+    public void buttonOperandOperation(View view){
+        TextView textView1 = findViewById(R.id.textView1);
+        switch (view.getId()) {
+            case R.id.buttonDiv:
+                if (displayValue.length() < 10 && !displayValue.endsWith("/") && !displayValue.endsWith("x") &&
+                        !displayValue.endsWith("+") && !displayValue.endsWith("-") && !displayValue.endsWith(".")) {
+                    displayValue = displayValue + "/";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.buttonMul:
+                if (displayValue.length() < 10 && !displayValue.endsWith("/") && !displayValue.endsWith("x") &&
+                        !displayValue.endsWith("+") && !displayValue.endsWith("-") && !displayValue.endsWith(".")) {
+                    displayValue = displayValue + "x";
+                    textView1.setText(displayValue);
+            }
+            break;
+
+            case R.id.buttonMin:
+                if (displayValue.length() < 10 && !displayValue.endsWith("/") && !displayValue.endsWith("x") &&
+                        !displayValue.endsWith("+") && !displayValue.endsWith("-") && !displayValue.endsWith(".")) {
+                    displayValue = displayValue + "-";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.buttonPlus:
+                if (displayValue.length() < 10 && !displayValue.endsWith("/") && !displayValue.endsWith("x") &&
+                        !displayValue.endsWith("+") && !displayValue.endsWith("-") && !displayValue.endsWith(".")) {
+                    displayValue = displayValue + "+";
+                    textView1.setText(displayValue);
+                }
+                break;
+
+            case R.id.buttonDot:
+                if (displayValue.length() < 10 && !displayValue.endsWith("/") && !displayValue.endsWith("x") &&
+                        !displayValue.endsWith("+") && !displayValue.endsWith("-") && !displayValue.endsWith(".")) {
+                    displayValue = displayValue + ".";
+                    textView1.setText(displayValue);
+                }
+                break;
+        }
+    }
+
 }
